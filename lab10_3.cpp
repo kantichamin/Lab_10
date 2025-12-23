e821f2c14a05211a7d8e530df9a40375f54d201d
Kanticha <kantian.0633717109@gmail.com>
2025-12-23 16:22:17 +0700

    data Lab_10

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    ifstream score1("score.txt");
+    double  Mean = 0 , de = 0;
+    int data = 0;
+    string text;
+    while(getline(score1,text)){
+        Mean += stod(text);
+        de += pow(stod(text),2);
+        data++;
+    }
+    score1.close();
+    
+    cout << "Number of data = " << data << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << Mean/data << "\n";
+    cout << "Standard deviation = " << sqrt(de/data - pow(Mean/data,2)) << "\n";
+    
+    return 0;
+}
